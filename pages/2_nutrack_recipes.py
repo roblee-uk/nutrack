df60c12599a595a09500738802e6fbc2b41ae782
Rob Lee <94rob.lee@gmail.com>
2025-02-16 14:43:54 +0000

    add new ingredient to existing recipe

--- pages/2_nutrack_recipes.py
+++ pages/2_nutrack_recipes.py
@@ -8,6 +8,10 @@ url: str = st.secrets["supabase"]["SUPABASE_URL"]
 key: str = st.secrets["supabase"]["SUPABASE_KEY"]
 supabase: Client = create_client(url, key)
 
+# ----------------------------------------------------------- #
+# Define Functions used in page ----------------------------- #
+# ----------------------------------------------------------- #
+
 # Function to fetch and display data
 def fetch_recipe_data():
     response = supabase.table("recipes").select("*").execute()
@@ -34,20 +38,64 @@ def fetch_recipe_ingredients(recipe_id):
 
     return clean_df[clean_df['recipe_id']==recipe_id][display_cols]
 
-recipes = fetch_recipe_data()
+# Function to populate a dataframe of all foods
+def fetch_food_data():
+    response = supabase.table("foods").select("*").execute()
+    df = pd.DataFrame(response.data)
+    return df
 
-# Display existing data
+# Preload data using functions defined (where appropriate)
+recipes = fetch_recipe_data()
+foods = fetch_food_data()
+
+# ---------------------------------------------------- #
+# Saved Recipes -------------------------------------- #
+# ---------------------------------------------------- #
+
+# Display recipe data
+# Allowing the user to select a single row at a time
 st.title("Saved Recipes")
 recipe_selection = st.dataframe(recipes['recipe_name']
                                 , hide_index=True
                                 , on_select="rerun"
                                 , selection_mode="single-row")
 
+# When a selection is made using the st.dataframe above
+# Save the recipe_id it to a variable that can be called later on
 selected_recipe = recipes['recipe_id'].iloc[recipe_selection['selection']['rows']]
 
+# If a selection has been made show the foods which make up the recipe selected
 if len(selected_recipe) > 0:
     id = selected_recipe.loc[0]
     st.dataframe(fetch_recipe_ingredients(id), hide_index=True)
+
+    # Form for adding food to recipe
+    st.subheader("Add New Food to Recipe")
+    with st.form("new_food_form", clear_on_submit=True,):
+        food_name = st.selectbox("Food Name", options=foods['food_name'])
+        amount = st.number_input("Amount (g)", min_value=0.0, step=0.1)
+        
+        
+        st.write(food_name)
+        submitted = st.form_submit_button("Add Food")
+
+        food_id = foods[foods['food_name']==food_name]['food_id'].to_list()[0]
+        st.write(selected_recipe.to_list()[0])
+        
+        if submitted:
+            new_ingredient = {
+                "food_id": food_id,
+                "recipe_id": selected_recipe.to_list()[0],
+                "amount": amount
+                }
+            
+            response = supabase.table("recipe_ingredients").insert(new_ingredient).execute()
+            
+            if response.data:
+                st.success(f"Successfully added {food_name} to the ingredients for {recipes[recipes['recipe_id']==selected_recipe]}!")
+                st.rerun()
+            else:
+                st.error("Failed to add food to the database. Please try again.")
 
 #st.dataframe(selected_ingredients[selected_ingredients['recipe_id']==selected_recipe])
 
@@ -55,37 +103,3 @@ if len(selected_recipe) > 0:
 
 
 
-""" # Form for new food entry
-st.subheader("Add New Food")
-with st.form("new_food_form", clear_on_submit=True,):
-    name = st.text_input("Food Name")
-    protein = st.number_input("Protein Content (g)", min_value=0.0, step=0.1)
-    carbs = st.number_input("Carbohydrate Content (g)", min_value=0.0, step=0.1)
-    sugars = st.number_input("of which sugars (g)", min_value=0.0, step=0.1)
-    fat = st.number_input("Fat Content (g)", min_value=0.0, step=0.1)
-    sats = st.number_input("of which saturates (g)", min_value=0.0, step=0.1)
-    fiber = st.number_input("Fiber Content (g)", min_value=0.0, step=0.1)
-    
-    submitted = st.form_submit_button("Add Food")
-    
-    if submitted:
-        current_time = datetime.now().isoformat()
-        new_food = {
-            "food_name": name,
-            "protein": protein,
-            "carbohydrates": carbs,
-            "sugars": sugars,
-            "fat": fat,
-            "saturates": sats,
-            "fiber": fiber,
-            "created_at": current_time,
-            "updated_at": current_time
-        }
-        
-        response = supabase.table("foods").insert(new_food).execute()
-        
-        if response.data:
-            st.success(f"Successfully added {name} to the database!")
-            st.rerun()
-        else:
-            st.error("Failed to add food to the database. Please try again.") """
